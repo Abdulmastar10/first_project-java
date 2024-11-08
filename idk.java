a27dbb222e8b0efe7253e03f509869f6a286e9e0
Abdulmastar10 <abdulh3852@gmail.com>
2024-11-07 16:50:18 -0800

    Update idk.java
    xdxdxd

--- idk.java
+++ idk.java
@@ -1,7 +1,9 @@
 import java.lang;
 import java.util.Scanner;
 
-public static void main(String args{})
+public class idk{
+public static void main(String[] args)
 {
     System.out.println("hello world");
+}
 }
